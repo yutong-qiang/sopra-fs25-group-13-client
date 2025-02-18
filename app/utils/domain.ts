93997f6191410527d773692f636851f466d4baf8
marcoleder <marco.leder@uzh.ch>
2025-02-18 22:37:53 +0100

    sacrificeLinter

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -1,4 +1,4 @@
-import process from "node:process";
+//import process from "node:process";
 import { isProduction } from "@/utils/environment";
 /**
  * Returns the API base URL based on the current environment.
