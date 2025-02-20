bbc10e8007ad674f3fa6bea58d41179e34e746d0
marcoleder <marco.leder@uzh.ch>
2025-02-20 17:02:50 +0100

    fixLogin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -30,7 +30,7 @@ const Login: React.FC = () => {
   const handleLogin = async (values: FormFieldProps) => {
     try {
       // Call the API service and let it handle JSON serialization and error handling
-      const response = await apiService.post<User>("/users", { values });
+      const response = await apiService.post<User>("/users", values);
 
       // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
       if (response.token) {
