7be68ce96054243dbb8f02679280522748ff08f1
ambros02 <44348096+ambros02@users.noreply.github.com>
2025-02-27 11:16:32 +0100

    added sample page for specific users
    fixed the problem where deno could not build, because it was missing a React.FC from the page.tsx inside the [id] folder

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -2,3 +2,22 @@
 // each user has their own slug /[id] (/1, /2, /3, ...) and is displayed using this file
 // try to leverage the component library from antd by utilizing "Card" to display the individual user
 // import { Card } from "antd"; // similar to /app/users/page.tsx
+
+"use client";
+// For components that need React hooks and browser APIs,
+// SSR (server side rendering) has to be disabled.
+// Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+
+import React from "react";
+
+const Profile: React.FC = () => {
+  return (
+    <div className="card-container">
+      <p>
+        <strong>SampleUser</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
