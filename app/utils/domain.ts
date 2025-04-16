48b0055f650bc7ef60948da134daf598c8978c44
Luca Bärtschiger <lucamarc.baertschiger@uzh.ch>
2025-04-16 11:23:22 +0200

    deleted unneeded results page

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "http://localhost:8080"; // TODO: update with your production URL as needed.
+      "https://sopra-fs25-group-13-server.oa.r.appspot.com";
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
